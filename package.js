5afc225e5eb5229d59556699d884502b98704883
rocky95002101-oss <rocky95002101@gmail.com>
2026-09-14 11:21:58 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,12 @@
+{
+  "name": "nuke-bot",
+  "version": "1.0.0",
+  "description": "Discord nuke bot",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^1^.14.1"
+  }
+}
